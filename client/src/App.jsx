c52b3f02ecce4709a7b29596c3a84b4dcf8f4a24
khalil <khalil3cheddadi@gmail.com>
2024-02-15 01:41:28 +0100

    done creating categories

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,71 +1,145 @@
-// import i18n from "./i18n";
-
 import { useState } from "react";
 
 function App() {
+  const colors = ["bg-red-300", "bg-orange-300", "bg-green-300", "bg-blue-300"];
   const [isOpen, setIsOpen] = useState(false);
+  const [category, setCategory] = useState({});
   const [questions, setQuestions] = useState([]);
-  const [categoryName, setCategoryName] = useState("");
   const [questionIndex, setQuestionIndex] = useState("");
   const [current, setCurrent] = useState("");
-  const [newCategoryOpen, setNewCategoryOpen] = useState(false);
-  const [newCategory, setNewCategory] = useState({ name: "", color: "" });
-  const colors = [];
-  const categories = [];
+  const [newCategoryOpen, setNewCategoryOpen] = useState({
+    isOpen: false,
+    index: null,
+  });
+  const [newCategory, setNewCategory] = useState({ name: "" });
+  const [categories, setCategories] = useState([]);
+
+  const valideNewCategory = (cat) => {
+    if (cat.name != "" && cat.color != "") return true;
+    else return false;
+  };
+
+  const createCategory = () => {
+    const data = {
+      name: newCategory.name,
+      color: colors[3 - categories.length],
+      questions: [],
+    };
+
+    const prev = categories;
+    prev.push(data);
+    setCategories(prev);
+    setNewCategoryOpen({
+      isOpen: false,
+      index: null,
+    });
+    setNewCategory({ name: "" });
+  };
 
   return (
     <>
       <div className="bg-blue-300 h-[100vh] w-full flex flex-col md:flex-row select-none ">
-        <div className="w-full md:w-1/4 md:max-w-lg h-1/4 md:h-full overflow-scroll bg-slate-700 flex flex-col gap-5 justify-center items-center">
-          <div className="p-5 m-5 absolute top-0 text-yellow-300  font-mono font-bold text-4xl capitalize">
+        <div
+          id="menu"
+          className="w-full md:w-1/4 md:max-w-lg h-1/4 md:h-full overflow-scroll bg-slate-700 flex flex-col gap-5 justify-center items-center"
+        >
+          <div
+            id="game-mode"
+            className="p-5 m-5 absolute top-0 text-yellow-300  font-mono font-bold text-4xl capitalize"
+          >
             edit mode
           </div>
           {categories.map((category, index) => (
             <div
+              key={index}
               onClick={() => {
+                setCategory(category);
                 setQuestions(category.questions);
-                setCategoryName(category.name);
               }}
               className={`w-1/2 h-16 ${category.color} hover:scale-110 duration-75 rounded-lg flex justify-center items-center font-bold text-xl cursor-pointer`}
             >
               {category.name}
             </div>
           ))}
-          <div
-            className={`w-1/2 ${
-              newCategoryOpen ? "h-32" : "h-0 border-0"
-            } overflow-hidden bg-gray-600 rounded-lg duration-150 ${
-              categories.length > 3 ? "hidden" : "flex"
-            }  justify-center items-center font-bold`}
-          >
-            <input
-              onChange={(e) =>
-                setNewCategory((prev) => ({
-                  name: e.target.value,
-                  color: prev.color,
-                }))
-              }
-              value={newCategory.name}
-              type="text"
-              placeholder="easy"
-              className="bg-transparent max-w-min text-center outline-none"
-            />
-          </div>
-          <div
-            className={`w-1/2 h-16 bg-gray-600 rounded-lg cursor-pointer  hover:scale-110 duration-75 ${
-              // categories.length > 3 || newCategoryOpen ? "hidden" : "flex"
-              "flex"
-            }  justify-center items-center font-bold text-4xl border-2 border-dashed text-white`}
-            onClick={() => setNewCategoryOpen(!newCategoryOpen)}
-          >
-            <p className={`${newCategoryOpen ? "rotate-45" : ""}`}>+</p>
-          </div>
+          {Array(4 - categories.length)
+            .fill()
+            .map((_, index) => (
+              <div
+                key={index}
+                className={`w-1/2 ${
+                  newCategoryOpen.isOpen && newCategoryOpen.index == index
+                    ? ""
+                    : "h-16 border border-dashed"
+                } overflow-hidden bg-gray-600 rounded-lg duration-150 flex flex-col gap-5 p-2 justify-center items-center font-bold relative`}
+              >
+                {newCategoryOpen.isOpen && newCategoryOpen.index == index ? (
+                  <>
+                    <input
+                      onChange={(e) =>
+                        setNewCategory((prev) => ({
+                          name: e.target.value,
+                          color: prev.color,
+                        }))
+                      }
+                      value={newCategory.name}
+                      type="text"
+                      placeholder="Category name"
+                      className="bg-transparent max-w-min text-center outline-none "
+                    />
+                    <div
+                      className={`w-3/4 h-8 flex items-center justify-center border-double border-4 border-black ${
+                        colors[3 - categories.length]
+                      }`}
+                    >
+                      color
+                    </div>
+                    <div className="flex gap-2">
+                      <div
+                        onClick={() => setNewCategoryOpen(false)}
+                        className={`relative bottom-0 right-0 p-2 rounded-md bg-red-400 cursor-pointer`}
+                      >
+                        close
+                      </div>
+                      <button
+                        onClick={
+                          newCategory.name != ""
+                            ? () => {
+                                createCategory();
+                              }
+                            : () => console.log("err")
+                        }
+                        className={`relative bottom-0 right-0 p-2 rounded-md  ${
+                          newCategory.name != ""
+                            ? "bg-green-400 cursor-pointer"
+                            : "bg-slate-400 "
+                        }`}
+                      >
+                        Create
+                      </button>
+                    </div>
+                  </>
+                ) : (
+                  <div
+                    className={`w-full h-full bg-gray-600 rounded-lg cursor-pointer  hover:scale-110 duration-75 flex justify-center items-center font-bold text-4xl border-2  text-white`}
+                    onClick={() =>
+                      setNewCategoryOpen({ isOpen: true, index: index })
+                    }
+                  >
+                    +
+                  </div>
+                )}
+              </div>
+            ))}
         </div>
-        <div className="h-3/4 md:h-full bg-gray-900 flex flex-grow gap-5 flex-wrap justify-center items-center relative p-24">
+        <div
+          id="questions"
+          className="h-3/4 md:h-full bg-gray-900 flex flex-grow gap-5 flex-wrap justify-center items-center relative p-24"
+        >
           <p className="absolute top-0 left-0 m-5 p-5 text-white text-3xl">
-            {categoryName}/{questionIndex}
+            {category.name}/{questionIndex}
           </p>
           <div
+            id="popup"
             className={`z-10 w-3/4 h-3/4 ${
               isOpen ? "flex" : "hidden"
             } bg-black rounded-lg absolute justify-center items-center text-white text-5xl`}
@@ -95,7 +169,7 @@ function App() {
           ))}
           <div
             className={`w-1/5 h-1/4 bg-gray-600 rounded-lg border-4 border-dashed  hover:scale-105 duration-150 cursor-pointer ${
-              questions.length > 11 || questions.length == 0 ? "hidden" : "flex"
+              category.length > 11 || category.length == 0 ? "hidden" : "flex"
             }  justify-center items-center font-bold text-4xl text-white`}
           >
             +
